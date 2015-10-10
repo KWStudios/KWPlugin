e4789d64159ac73b998d0cca166a405dc6196eac
Ybrin <koray@koska.at>
2015-10-10 21:40:55 +0200

    Changed server address to https

--- src/org/kwstudios/play/toolbox/ConstantHolder.java
+++ src/org/kwstudios/play/toolbox/ConstantHolder.java
@@ -7,6 +7,6 @@ public class ConstantHolder {
 	public static final String MOTD_PREFIX = ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD.toString() + "KWStudios"
 			+ ChatColor.RESET + ":" + " " + ChatColor.GOLD + ChatColor.ITALIC;
 	
-	public static final String API_URL = "http://api.kwstudios.org/minecraft/server/:server/players/:player/storedata";
+	public static final String API_URL = "https://api.kwstudios.org/minecraft/server/:server/players/:player/storedata";
 
 }
